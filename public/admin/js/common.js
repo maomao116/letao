124d35a6aab60709f10187028e285d7674524301
maomao116 <573947632@qq.com>
2018-07-13 20:21:06 +0800

    进度条完成

--- public/admin/js/common.js
+++ public/admin/js/common.js
@@ -27,3 +27,17 @@ $(function(){
     })
   })
 })
+NProgress.configure({ showSpinner: false });
+
+//注册一个全局的ajaxStart事件，所有的ajax在开启的时候，会触发这个事件
+$(document).ajaxStart(function () {
+  //开启进度条
+  NProgress.start();
+});
+
+$(document).ajaxStop(function () {
+  //完成进度条
+  setTimeout(function () {
+    NProgress.done();
+  }, 500);
+});
